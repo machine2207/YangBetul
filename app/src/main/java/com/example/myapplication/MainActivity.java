fe26d92c86671dd1cd9b9b915fd6fef6d29ad2eb
machine2207 <gun.machine22072003@gmailcom>
2023-05-27 01:50:03 +0800

    UPDATE WOII

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -3,6 +3,10 @@ package com.example.myapplication;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,19 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        EditText editTextWeight = findViewById(R.id.weight2);
+        EditText editTextHeight = findViewById(R.id.height2);
+        Button button = findViewById(R.id.btnSubmit);
+        TextView textView = findViewById(R.id.result);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                float height = Float.parseFloat(String.valueOf(editTextHeight.getText())) / 100;
+                float weight = Float.parseFloat(String.valueOf(editTextWeight.getText()));
+                float bmi = weight / (height * height);
+                textView.setText(String.valueOf(bmi));
+            }
+        });
     }
 }
